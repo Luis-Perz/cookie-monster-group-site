7823dd9135aa7478c5c864f1e032f4d16158fcf2
Luis Perez <82295850+Luis-Perz@users.noreply.github.com>
2026-03-11 19:32:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+// HAMBURGER MENU (mobile menu)
+const hamMenu = document.querySelector('.ham-menu');
+const hiddenMenu = document.querySelector('.hidden-menu');
+const navLink = document.querySelectorAll('.nav-link');
+const introContainer = document.querySelector('.intro-container');
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    if (!hiddenMenu.classList.toggle('active')){
+        hiddenMenu.style.display = "none";
+    }else{
+        hiddenMenu.style.display = "flex";
+        document.body.style.overflow = '';
+        navLink.forEach(link => link.addEventListener("click", () => {
+            hiddenMenu.style.display = "none";
+            hamMenu.classList.remove('active');
+            hiddenMenu.classList.remove('active');
+        }))
+        introContainer.addEventListener("click", () => {
+        hiddenMenu.style.display = "none";
+        hamMenu.classList.remove('active');
+        hiddenMenu.classList.remove('active');
+        })
+    }
+
+});
+
+const scrollers = document.querySelectorAll(".scroller");
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches){
+    addAnimation();
+}
+
+function addAnimation(){
+    scrollers.forEach(scroller => {
+        scroller.setAttribute("data-animated", true);
+        const scrollerInner = scroller.querySelector(".scroller-cont");
+        const scrollerContent = Array.from(scrollerInner.children);
+        scrollerContent.forEach(item =>{
+            const duplicateItem = item.cloneNode(true);
+            duplicateItem.setAttribute("aria-hidden", true)
+            scrollerInner.appendChild(duplicateItem)
+        });
+    })
+}
+
+// PROJECTS SECTION--- rendering project cards
+fetch('JSON/projects.json')
+    .then(res => res.json())
+    .then(projects => {
+        const container = document.getElementById('project');
+        projects.forEach(project => {
+            const card = document.createElement('div');
+            card.className = 'item-card';
+            card.innerHTML = `    
+                <img class="card-img" src="${project.image}" alt="project-img">
+                <div class="card-info">
+                    <h1 class="card-title">${project.title}</h1>
+                    <p class="card-description">${project.description}</p>
+                    <a class="card-link" href="#">View Project</a>
+                </div>
+            `;
+        container.appendChild(card);
+        });
+    });
+
+// MEMBERS SECTION
+fetch('JSON/members.json')
+    .then(res => res.json())
+    .then(members => {
+        const container = document.getElementById('member');
+        members.forEach(member => {
+            const card = document.createElement('div');
+            card.className = 'item-card';
+            card.innerHTML = `
+                <img class="card-img" src="${member.image}" alt="project-img"></img>
+                <div class="card-info">
+                    <h1 class="card-name">${member.name}</h1>
+                    <p class="card-role">${member.name}</p>
+                    <a class="card-link" href="#">About Me</a>
+                </div>
+            `;
+            container.appendChild(card);
+        })
+    })
